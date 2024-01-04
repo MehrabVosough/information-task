a482d71401d92c536282640fb0e0c6f0b74c189e
Mehrab vosough <mehrabvosough.dp@gmail.com>
2024-01-04 18:24:23 +0330

    event listener ADDED too marital status

--- js/personal.js
+++ js/personal.js
@@ -1,8 +1,18 @@
 const genderSel = document.querySelector("#gender");
 const military = document.querySelector(".military");
+const maritalStatus = document.querySelector("#maritalStatus");
+const numChildren = document.querySelector(".num-children");
 
 genderSel.addEventListener("change", () => {
 	genderSel.value === "male"
 		? (military.style.display = "block")
 		: (military.style.display = "none");
 });
+
+maritalStatus.addEventListener("change", () => {
+	maritalStatus.value === "married"
+		? (numChildren.style.display = "block")
+		: (numChildren.style.display = "none");
+});
+
+
