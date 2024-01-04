a5f942d33a22a76b3e75d7be423bfdc3081b5aa7
Mehrab vosough <mehrabvosough.dp@gmail.com>
2024-01-04 20:19:30 +0330

    add a EventListener to ages and Loop To make age elements equal to the number of children

--- js/personal.js
+++ js/personal.js
@@ -2,6 +2,8 @@ const genderSel = document.querySelector("#gender");
 const military = document.querySelector(".military");
 const maritalStatus = document.querySelector("#maritalStatus");
 const numChildren = document.querySelector(".num-children");
+const numChildrenInput = document.querySelector(".numberOfChildren");
+const ageDiv = document.querySelector(".ages");
 
 genderSel.addEventListener("change", () => {
 	genderSel.value === "male"
@@ -15,4 +17,15 @@ maritalStatus.addEventListener("change", () => {
 		: (numChildren.style.display = "none");
 });
 
-
+numChildrenInput.addEventListener("change", () => {
+	const childrenCounter = +numChildrenInput.value;
+	console.log(childrenCounter);
+	ageDiv.style.display = "inline-block";
+	for (let i = 1; i <= childrenCounter; i++) {
+		const div = document.createElement("div");
+		const markUp = `<label for="childrenAges">سن فرزند${i}:</label>
+		<input type="number" id="childrenAges" name="childrenAges" required />`;
+		div.innerHTML = markUp;
+		ageDiv.appendChild(div);
+	}
+});
