7012044777b4b621cb62df8086bf81cbe6a1ed2b
Mehrab vosough <mehrabvosough.dp@gmail.com>
2024-01-05 15:12:08 +0330

    The maintenance of the entered information is complete (part two) | issues: Dependent elements

--- js/personal.js
+++ js/personal.js
@@ -39,7 +39,7 @@ numChildrenInput.addEventListener("change", () => {
 	}
 });
 
-// SAVE TOO LOCAL
+// SAVE TO LOCAL
 const saveToLocal = submit.addEventListener("click", () => {
 	const saving = [
 		{
@@ -57,6 +57,31 @@ const saveToLocal = submit.addEventListener("click", () => {
 	localStorage.setItem("information", JSON.stringify(saving));
 });
 
+//destructure for keep data
+
 const savedLocal = localStorage.getItem("information");
 const pareseSaveLocal = JSON.parse(savedLocal);
-console.log(pareseSaveLocal);
+const destructuring = pareseSaveLocal[0];
+const keys = Object.values(destructuring);
+
+const [
+	name,
+	lastName,
+	phone,
+	email,
+	marital,
+	gender,
+	city,
+	military,
+	children,
+] = keys;
+
+nameInput.value = name;
+lastNameInput.value = lastName;
+phoneInput.value = phone;
+emailInput.value = email;
+maritalStatus.value = marital;
+genderSel.value = gender;
+citySel.value = city;
+militarySel.value = military;
+numChildrenInput.value = children;
