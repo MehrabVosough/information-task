68e7f946fcfd2d546f1cfa4e817a1cf21cb2a001
Mehrab vosough <mehrabvosough.dp@gmail.com>
2024-01-05 12:40:03 +0330

    Save to localStorage Handled and Parsed data to ArrayOfObject (Part one)

--- js/personal.js
+++ js/personal.js
@@ -1,14 +1,23 @@
+// GETS
 const genderSel = document.querySelector("#gender");
-const military = document.querySelector(".military");
+const militaryDev = document.querySelector(".military");
+const militarySel = document.querySelector("#militaryService");
 const maritalStatus = document.querySelector("#maritalStatus");
 const numChildren = document.querySelector(".num-children");
 const numChildrenInput = document.querySelector(".numberOfChildren");
 const ageDiv = document.querySelector(".ages");
+const nameInput = document.querySelector("#firstName");
+const lastNameInput = document.querySelector("#lastName");
+const phoneInput = document.querySelector("#phoneNumber");
+const emailInput = document.querySelector("#email");
+const citySel = document.querySelector("#city");
+const submit = document.querySelector(".submit");
 
+// DEPENCE
 genderSel.addEventListener("change", () => {
 	genderSel.value === "male"
-		? (military.style.display = "block")
-		: (military.style.display = "none");
+		? (militaryDev.style.display = "block")
+		: (militaryDev.style.display = "none");
 });
 
 maritalStatus.addEventListener("change", () => {
@@ -24,8 +33,30 @@ numChildrenInput.addEventListener("change", () => {
 	for (let i = 1; i <= childrenCounter; i++) {
 		const div = document.createElement("div");
 		const markUp = `<label for="childrenAges">سن فرزند${i}:</label>
-		<input type="number" id="childrenAges" name="childrenAges" required />`;
+		<input  type="text" step="1" id="childrenAges" name="childrenAges" required />`;
 		div.innerHTML = markUp;
 		ageDiv.appendChild(div);
 	}
 });
+
+// SAVE TOO LOCAL
+const saveToLocal = submit.addEventListener("click", () => {
+	const saving = [
+		{
+			name: nameInput.value,
+			lastNameInput: lastNameInput.value,
+			phoneInput: phoneInput.value,
+			emailInput: emailInput.value,
+			maritalStatus: maritalStatus.value,
+			gender: genderSel.value,
+			citySel: citySel.value,
+			military: militarySel.value,
+			numChildrenInput: numChildrenInput.value,
+		},
+	];
+	localStorage.setItem("information", JSON.stringify(saving));
+});
+
+const savedLocal = localStorage.getItem("information");
+const pareseSaveLocal = JSON.parse(savedLocal);
+console.log(pareseSaveLocal);
